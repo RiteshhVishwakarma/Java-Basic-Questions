05f7b8e78af34e0c314d367d9e680179a9371479
Ritesh Vishwakarma <113338495+RiteshhVishwakarma@users.noreply.github.com>
2025-02-24 15:09:16 +0530

    Study about Concepts of Strings And Array

---
+++ StringReverse.java
@@ -0,0 +1,15 @@
+public class StringReverse {
+    public static void main(String[] args) {
+
+        String rev = "", str = "HelloWorld";
+        char ch;
+
+        System.out.println("Original: "+str);
+        for (int i = 0; i<str.length(); i++){
+            ch = str.charAt(i);
+            rev = ch + rev;
+        }
+        System.out.println("Reverse: "+rev);
+
+    }
+}
